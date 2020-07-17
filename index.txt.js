71aeda8ecfbcfaff08144682be44feaa2b943c22
chanhee65 <68429344+chanhee65@users.noreply.github.com>
2020-07-17 21:48:01 +0900

    Add files via upload

---
+++ index.txt.js
@@ -0,0 +1,12 @@
+﻿var time = prompt('몇초마다 작품을 업데이트할지 정하세요','5')
+while(time<5){
+  time = prompt('5초 이상만 가능합니다.','5')
+}
+  function start() {
+  $('.editDescriptionBtn').trigger('click')
+  $('.editConfirmBtn').trigger('click')
+  console.log('작품이 업데이트되었습니다.')
+}
+alert('avo 매크로가 실행되었습니다.')
+start()
+setInterval(start, time*1000)
